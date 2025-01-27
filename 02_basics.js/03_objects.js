2860b93e20e0d4e405cd7ddfceeed15738c7ad49
Shubham3803 <Work.shubham1601@gmail.com>
2025-01-27 14:53:05 +0530

    Object

---
+++ 02_basics.js/03_objects.js
@@ -0,0 +1,40 @@
+//  singleton
+
+//  object literals way to write a object 
+
+// Object.create
+
+const mysymble =Symbol("key1")
+
+const Jsuder ={
+  name: "shubham",
+  "full name": "shubham Bhardwaj",
+  [mysymble]: "key1",
+  age:24,
+  location:"ghaziabad",
+  email:"shbham@google.com",
+  isLoggedIn: false,
+  lastLoginDays:["monday", "saturday"]
+}
+
+// console.log(Jsuder.email);
+// console.log(Jsuder["email"]);
+// console.log(Jsuder["full name"]);
+// console.log(Jsuder[mysymble]);
+
+Jsuder.email ="shubham@chatgpt.com"
+// Object.freeze(Jsuder)
+Jsuder.email ="shubham123"
+// console.log(Jsuder);
+
+Jsuder.greeting = function(){
+  console.log("hlo user");
+}
+
+
+Jsuder.greeting2 = function(){
+  console.log(`hlo user, ${this.name}`);
+}
+
+console.log(Jsuder.greeting());
+console.log(Jsuder.greeting2());
