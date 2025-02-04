bee9b3d495f9c2986986c8690fb94bb57dc93391
Shubham3803 <Work.shubham1601@gmail.com>
2025-02-04 14:21:06 +0530

    scope level

--- 03_Basics.js/02_Scopes.js
+++ 03_Basics.js/02_Scopes.js
@@ -5,12 +5,57 @@ let a =300
 if(true){
   let a =10
 const b = 20
-console.log("Inner:", a);
+// console.log("Inner:", a);
 
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one (){
+ const username = "Shubham"
+ 
+ 
+ function two(){
+  const website = "Youtube"
+  console.log(username);
+ }
+//  console.log(website);
+
+//  two()
+ 
+}
+
+// one()
+if (true) {
+  const username= "Shubham"
+  if (username === "Shubham") {
+    const website =" Youtube"
+    // console.log(username + website);
+    
+  }
+  // console.log(website);
+  
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++ intresting +++++++++++++
+
+
+function addone(num){
+  return num + 1
+}
+console.log(addone(5))
+
+
+const addtwo = function(num){
+  return num +2
+}
+console.log(addtwo(5))
